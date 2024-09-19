fcf9fa8a94315f17a13a618232a147971f2019aa
firegory <85395729+firegory@users.noreply.github.com>
2024-09-19 17:44:45 +0300

    Задачи с урока 19.09 решены

---
+++ prj.codeforces/1717b.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    int t = 0;
+    int n = 0;
+    int koef = 0;
+    int x = 0;
+    int y = 0;
+    std::cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> n;
+        std::cin >> koef;
+        std::cin >> x;
+        std::cin >> y;
+        x -= 1;
+        y -= 1;
+        char** mas = new char* [n];
+        for (int j = 0; j < n; ++j)
+        {
+            mas[j] = new char[n];
+            for (int k = 0; k < n; k++)
+            {
+                mas[j][k] = '.';
+            }
+        }
+        for (int j = 0; j < n; ++j)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                if (((j - k) % koef + koef) % koef == ((x - y) % koef + koef) % koef)
+                {
+                    mas[j][k] = 'X';
+                }
+            }
+        }
+
+
+
+        for (int j = 0; j < n; ++j)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                std::cout << mas[j][k];
+            }
+            std::cout << "\n";
+        }
+
+        for (int j = 0; j < n; ++j) delete[] mas[j];
+        delete[] mas;
+    }
+
+}
