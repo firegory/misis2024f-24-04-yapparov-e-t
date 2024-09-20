496a7a0172aca60959d7b3ccd1e50a5d5e53fdbe
firegory <85395729+firegory@users.noreply.github.com>
2024-09-20 18:02:56 +0300

    Раз появился Мисис гитхаб, почему бы не хранить тут ВычМаш

---
+++ ВычМач/ImageApp/ImageApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace ImageApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "See Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(205, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(583, 405);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ВычМач/ImageApp/ImageApp/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            short[,,] mas = readIm(textBox1.Text);
+            //showImage(mas);
+            saveImage(mas, "");
+        }
+        private short[,,] readIm(string path)
+        {
+            try
+            {
+                Bitmap image1 = new Bitmap(textBox1.Text, true);
+
+                int x, y;
+                short[,,] mas = new short[image1.Width, image1.Height, 3];
+                for (x = 0; x < image1.Width; x++)
+                {
+                    for (y = 0; y < image1.Height; y++)
+                    {
+                        Color pixelColor = image1.GetPixel(x, y);
+                        mas[x, y, 0] = pixelColor.R;
+                        mas[x, y, 1] = pixelColor.G;
+                        mas[x, y, 2] = pixelColor.B;
+                    }
+                }
+
+                return mas;
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                    "Check the path to the image file.");
+                return null;
+            }
+        }
+        private void showImage(short[,,] mas)
+        {
+            Bitmap image1 = new Bitmap(mas.GetLength(0), mas.GetLength(1));
+            int x, y;
+            for (x = 0; x < image1.Width; x++)
+            {
+                for (y = 0; y < image1.Height; y++)
+                {
+                    Color newColor = Color.FromArgb(mas[x,y,0], mas[x, y, 1], mas[x, y, 2]);
+                    image1.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox1.Image = image1;
+
+        }
+        private void saveImage(short[,,] mas, string name)
+        {
+            List<Color> colors = new List<Color>();
+
+            int x, y, k;
+            bool a;
+            for (x = 0; x < mas.GetLength(0); x++)
+            {
+                for (y = 0; y < mas.GetLength(1); y++)
+                {
+                    a = true;
+                    for (k = 0; k < colors.Count(); k++)
+                    {
+                        if (colors[k].R == mas[x,y,0] && colors[k].G == mas[x, y, 1] && colors[k].B == mas[x, y, 2])
+                        {
+                            a = false;
+                        }
+                    }
+                    if (a)
+                    {
+                        Color newColor = Color.FromArgb(mas[x, y, 0], mas[x, y, 1], mas[x, y, 2]);
+                        colors.Add(newColor);
+                    }
+                }
+            }
+            MessageBox.Show(colors.Count().ToString());
+        }
+
+    }
+}
