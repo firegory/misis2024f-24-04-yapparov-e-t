3a4c68accd601cf6506d9f75ebc94df5270eb5bb
firegory <85395729+firegory@users.noreply.github.com>
2024-09-23 19:45:21 +0300

    Дз

---
+++ prj.codeforces/2004B.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    int t = 0;
+    std::cin >> t;
+    int l = 0;
+    int r = 0;
+    int l1 = 0;
+    int r1 = 0;
+    int ans = 0;
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> l;
+        std::cin >> r;
+        std::cin >> l1;
+        std::cin >> r1;
+        ans = std::min(r,r1) - std::max(l1,l);
+        if (ans+1 <= 0)
+        {
+            std::cout << 1 << "\n";
+            continue;
+        }
+        if (l != l1)
+        {
+            ans += 1;
+        }
+        if (r != r1)
+        {
+            ans += 1;
+        }
+        std::cout << ans << "\n";
+    }
+}
---
+++ prj.codeforces/2014H.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+//Взялся за задачку H, не понимаю как дальше оптимимзировать(саму задачку тестам решил)
+
+int partition(int** vec, int low, int high) 
+{
+    int pivot = vec[0][high];
+
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+
+        if (vec[0][j] <= pivot) {
+            i++;
+            std::swap(vec[0][i], vec[0][j]);
+            std::swap(vec[1][i], vec[1][j]);
+        }
+    }
+
+    std::swap(vec[0][i + 1], vec[0][high]);
+    std::swap(vec[1][i + 1], vec[1][high]);
+
+    return (i + 1);
+}
+
+void quickSort(int**& vec, int low, int high) 
+{
+    if (low < high) {
+        int pi = partition(vec, low, high);
+
+        quickSort(vec, low, pi - 1);
+        quickSort(vec, pi + 1, high);
+    }
+}
+
+
+int main() {
+    int t = 0;
+    std::cin >> t;
+    int n = 0;
+    int q = 0;
+    int** a = new int* [2];
+    int temp = 0;
+    int l = 0;
+    int r = 0;
+    int rCount = 0;
+    int shCount = 0;
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> n;
+        std::cin >> q;
+        a[0] = new int[n];
+        a[1] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            std::cin >> a[0][j];
+            a[1][j] = j;
+        }
+        quickSort(a, 0, n - 1);
+        for (int j = 0; j < q; j++)
+        {
+            rCount = 0;
+            shCount = 0;
+            std::cin >> l;
+            std::cin >> r;
+            temp = 0;
+            for (int k = 0; k < n; k++)
+            {
+                if (a[1][k] >= l - 1 && a[1][k] <= r - 1)
+                {
+                    if (temp % 2 == 0)
+                    {
+                        rCount += a[0][k];
+                    }
+                    else
+                    {
+                        shCount += a[0][k];
+                    }
+                    temp += 1;
+                    if (temp == r - l + 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (rCount == shCount)
+            {
+                std::cout << "YES\n";
+            }
+            else
+            {
+                std::cout << "NO\n";
+            }
+        }
+    }
+}
