23da631d8dba2479f65312eed483fb79f2b8906c
firegory <85395729+firegory@users.noreply.github.com>
2024-10-21 09:44:34 +0300

    +2 codeforces tasks

---
+++ prj.codeforces/2022a.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main() {
+    int t = 0;
+    std::cin >> t;
+    int n = 0;
+    int r = 0;
+    int temp = 0;
+    int placesLeft = 0;
+    int aloneSits = 0;
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> n >> r;
+        placesLeft = r * 2;
+        aloneSits = 0;
+        for (int j = 0; j < n; j++)
+        {
+            std::cin >> temp;
+            placesLeft -= (temp - temp % 2);
+            aloneSits += temp % 2;
+        }
+        if (aloneSits <= placesLeft/2)
+        {
+            std::cout << (r * 2) - placesLeft + aloneSits << "\n";
+        }
+        else
+        {
+            std::cout << (r * 2) - placesLeft + aloneSits - (aloneSits - placesLeft / 2) * 2 << "\n";
+        }
+    }
+}
---
+++ prj.codeforces/2024a.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main() {
+    int t = 0;
+    std::cin >> t;
+    int a = 0;
+    int b = 0;
+    int temp = 0;
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> a >> b;
+        if (a >= b)
+        {
+            std::cout << a << "\n";
+        }
+        else if (2*a <= b)
+        {
+            std::cout << 0 << "\n";
+        }
+        else
+        {
+            std::cout << a - (b - a) << "\n";
+        }
+    }
+}
