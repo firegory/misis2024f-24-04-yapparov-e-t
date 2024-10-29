eb804a040c4007f7e810b87c1f4d0a490b4bef7a
firegory <85395729+firegory@users.noreply.github.com>
2024-10-29 09:17:29 +0300

    2 more codforces tasks

---
+++ prj.codeforces/2033a.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main() {
+    int t = 0;
+    std::cin >> t;
+    int n = 0;
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> n;
+        if (n%2==0)
+        {
+            std::cout << "Sakurako\n";
+        }
+        else
+        {
+            std::cout << "Kosuke\n";
+        }
+    }
+}
---
+++ prj.codeforces/2033b.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    int t = 0;
+    std::cin >> t;
+    int n = 0;
+    std::vector<std::vector<int>> mas;
+    int adder = 0;
+    long long result = 0;
+    int temp = 0;
+    std::vector<int> tempv;
+    tempv.push_back(0);
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> n;
+        mas.clear();
+        result = 0;
+        for (int j = 0; j < n * 2 - 1; j++)
+        {
+            mas.push_back(tempv);
+        }
+        for (int j = 0; j < n*n; j++)
+        {
+            std::cin >> temp;
+
+            mas[n - 1 + j / n - j % n].push_back(temp);
+        }
+        for (int j = 0; j < n * 2 - 1; j++)
+        {
+            adder = 0;
+            for (int k = 0; k < mas[j].size(); k++)
+            {
+                if (mas[j][k] < -1*adder)
+                {
+                    adder = -1 * mas[j][k];
+                }
+            }
+            result += adder;
+        }
+        std::cout << result << "\n";
+    }
+}
